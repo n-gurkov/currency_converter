8c0d12e3a0ea769f9e538d58370ea77892dcc624
Никита Гурьков <gurkov.nikita@gmail.com>
2022-03-21 17:33:12 +0300

    Update BasicTextFields.jsx

--- src/components/BasicTextFields.jsx
+++ src/components/BasicTextFields.jsx
@@ -1,14 +1,28 @@
-import * as React from 'react';
+
+import React, { useRef, Component } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import '../index.css'
 
-export default function BasicTextFields() {
+export default function BasicTextFields(props) {
+  const [field, setField] = React.useState('');
+  const valueRef = useRef('')
+  const handleChange = (event) => {
+    setField(event.target.value);
+    console.log(event.target.value)
+   
+  };
+  const rate = 2
+  let newField = field * rate
   return (
-    <Box className="App-inputs">
+    
+    <Box className="App-inputs"
+    onChange={handleChange}
+    >
 
-      <TextField/>
+      <TextField value = {newField} inputRef={valueRef}  />
+      
     </Box>
   );
 }
