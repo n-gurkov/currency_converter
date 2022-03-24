ea77c1ca9c8081a58d27264ddef9d98542ee4b61
Никита Гурьков <gurkov.nikita@gmail.com>
2022-03-24 11:53:34 +0300

    сamelCase changes

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/400.css";
 
 function App() {
-  const [currencyFrom, setcurrencyFrom] = useState(1);
-  const [currencyTo, setcurrencyTo] = useState(1);
-  const [valueFrom, setvalueFrom] = useState("");
-  const [valueTo, setvalueTo] = useState("");
+  const [currencyFrom, setCurrencyFrom] = useState(1);
+  const [currencyTo, setCurrencyTo] = useState(1);
+  const [valueFrom, setValueFrom] = useState("");
+  const [valueTo, setValueTo] = useState("");
 
   const counter = (event, flag) => {
     if (flag === true) {
-      setvalueFrom(event);
-      setvalueTo(((event / currencyFrom) * currencyTo).toFixed(4)-0);
+      setValueFrom(event);
+      setValueTo(((event / currencyFrom) * currencyTo).toFixed(4) - 0);
     } else {
-      setvalueTo(event);
-      setvalueFrom(((event / currencyTo) * currencyFrom).toFixed(4)-0);
+      setValueTo(event);
+      setValueFrom(((event / currencyTo) * currencyFrom).toFixed(4) - 0);
     }
   };
 
   const valuesChange = () => {
-    setvalueFrom(valueTo);
-    setvalueTo(valueFrom);
-    setcurrencyTo(currencyFrom);
-    setcurrencyFrom(currencyTo);
+    setValueFrom(valueTo);
+    setValueTo(valueFrom);
+    setCurrencyTo(currencyFrom);
+    setCurrencyFrom(currencyTo);
   };
 
   const [allValues, setAllValues] = useState([]);
@@ -63,19 +63,19 @@ function App() {
             <Grid item xs={3.2}>
               <CurrencySelect
                 items={allValues}
-                setSelect={setcurrencyFrom}
+                setSelect={setCurrencyFrom}
                 value={currencyFrom}
               />
             </Grid>
 
             <Grid item xs={0.5}>
-              <div className="MainText">в </div>
+              <div className="MainText">в</div>
             </Grid>
 
             <Grid item xs={3}>
               <CurrencySelect
                 items={allValues}
-                setSelect={setcurrencyTo}
+                setSelect={setCurrencyTo}
                 value={currencyTo}
               />
             </Grid>
@@ -95,7 +95,11 @@ function App() {
             </Grid>
 
             <Grid item xs={3}>
-              <BasicTextFields counter={counter} value={valueTo} flag={false} />
+              <BasicTextFields 
+                counter={counter} 
+                value={valueTo} 
+                flag={false} 
+              />
             </Grid>
           </Grid>
 
